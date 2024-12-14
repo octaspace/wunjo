03c0f1bd661c0f20760dba871a6fe63d636b4047
Vladislav <maksic1996@yandex.ru>
2024-12-14 18:19:17 +0300

    remove lws

--- deepfake/src/utils/audio.py
+++ deepfake/src/utils/audio.py
@@ -77,15 +77,8 @@ def melspectrogram(wav):
         return _normalize(S)
     return S
 
-def _lws_processor():
-    import lws
-    return lws.lws(hp.n_fft, get_hop_size(), fftsize=hp.win_size, mode="speech")
-
 def _stft(y):
-    if hp.use_lws:
-        return _lws_processor(hp).stft(y).T
-    else:
-        return librosa.stft(y=y, n_fft=hp.n_fft, hop_length=get_hop_size(), win_length=hp.win_size)
+    return librosa.stft(y=y, n_fft=hp.n_fft, hop_length=get_hop_size(), win_length=hp.win_size)
 
 ##########################################################
 #Those are only correct when using lws!!! (This was messing with Wavenet quality for a long time!)
